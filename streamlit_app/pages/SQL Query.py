a72ece9445226cba5bac321edea561a027005db9
sneccello <marcellschneider@icloud.com>
2025-01-04 18:41:52 +0100

    add query explain

--- streamlit_app/pages/SQL Query.py
+++ streamlit_app/pages/SQL Query.py
@@ -1,29 +1,33 @@
 import streamlit as st
 from code_editor import code_editor
+from pyspark.errors import ParseException
 from pyspark.shell import spark
 
 from pages.Overview import display_filter
 from session_utils import get_spark_session, get_config, SessionMeta
-from spark_utils import get_normalized_df
+from spark_utils import get_normalized_df, COLUMNS
+
+TABLE_NAME = 'commits'
+DEFAULT_QUERY = \
+        """
+       SELECT author, count(*) as n_created_files
+       FROM (
+           SELECT author, explode(files) as files FROM commits
+       )
+       WHERE files.status = 'A'
+       group by author
+       ORDER BY n_created_files DESC
+       LIMIT 5;
+       
+       -- Select Merge commits
+       --SELECT * from commits
+       --where size(parents) > 1
+       """
 
 def display_editor_space():
 
     if not SessionMeta.get_selected_repositories():
         st.write("##### No data found :( Add and/or Select repositories to visualize!")
-
-
-    TABLE_NAME = 'commits'
-    DEFAULT_QUERY = """
-           -- List authors with most files created
-           SELECT author, count(*) as n_created_files
-           FROM (
-               SELECT author, explode(files) as files FROM commits
-           )
-           WHERE files.status = 'A'
-           group by author
-           ORDER BY n_created_files DESC
-           LIMIT 5 
-           """
 
     commits = get_normalized_df(get_spark_session(), get_config(), SessionMeta.get_selected_repositories())
 
@@ -39,12 +43,25 @@ def display_editor_space():
         lang="sql",
         height=len(DEFAULT_QUERY.split('\n')),
         ghost_text='asda',
-        completions=[TABLE_NAME]
+        completions=[TABLE_NAME, *COLUMNS.get_values()]
     )
-    run = st.button('Run Query (CTRL + Enter)')
+
+    st.write("_Press CTRL+ENTER to RUN_")
+
     st.write('## Query Result')
-    if run or response['type'] == 'submit' or response['type'] == '':  # first run
+    if response['type'] == 'submit' or response['type'] == '':  # first run
         query = response['text'] or DEFAULT_QUERY
+
+        with st.expander('Explain Query Plan', expanded=False):
+            st.write("Plan:")
+            st.write(f"_{response['text']}_")
+            try:
+                st.write(spark.sql("EXPLAIN FORMATTED " + query).collect()[0].plan)
+            except ParseException as e:
+                st.write("Failed to analyze plan")
+                st.write(e)
+                st.write("Query: \n" + query)
+
         commits.createOrReplaceTempView("commits")
         query_df = spark.sql(query)
         st.write(query_df)
